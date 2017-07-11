891d7da3cf54350ebe56764da4f802871eaa2863
Ramisetty-Srinivas <vaasu619@gmail.com>
2017-07-11 20:42:18 +0530

    ACD_JAVAB_Session_9_Assignment_2

---
+++ src/session9_assignment2/SetStuff.java
@@ -0,0 +1,40 @@
+package session9_assignment2;
+import java.util.*;
+
+public class SetStuff {
+
+/** Show the union and instersection of two sets. */
+  public static void main(String[] args) {
+
+    // Create two sets.
+    Set s1 = new HashSet();
+    s1.add("Ian Darwin");
+    s1.add("Bill Dooley");
+    s1.add("Jesse James");
+
+    Set s2 = new HashSet();
+    s2.add("Ian Darwin");
+    s2.add("Doolin' Dalton");
+
+    Set union = new TreeSet(s1);
+    union.addAll(s2);    // now contains the union
+
+    print("union", union);
+
+    Set intersect = new TreeSet(s1);
+    intersect.retainAll(s2);
+
+    print("intersection", intersect);
+
+  }
+
+  protected static void print(String label, Collection c) {
+
+    System.out.println("--------------" + label + "--------------");
+
+    Iterator it = c.iterator();
+    while (it.hasNext()) {
+      System.out.println(it.next());
+    }
+  }
+}
